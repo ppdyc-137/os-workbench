d4122298f396f6ae6819c0ec5b505f30210d070a
wsl-arch <1060927911@qq.com>
2024-07-17 22:32:34 +0800

    M4 - crepl finish

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,8 +1,56 @@
+#include <dlfcn.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define WRAPPER_NAME_FMT "_expr_wrapper_%d"
+#define WRAPPER_FMT "int _expr_wrapper_%d() { return %s; }"
+
+int build_shared_lib(const char *filename, const char *libname) {
+    pid_t pid = fork();
+    if(pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0) {
+        int dev_null = open("/dev/null", O_WRONLY);
+        if (dev_null < 0) {
+            perror("open");
+        } else {
+            dup2(dev_null, STDOUT_FILENO);
+            dup2(dev_null, STDERR_FILENO);
+            close(dev_null);
+        }
+#if __x86_64__
+        execlp("gcc", "gcc", "-fPIC", "-shared", "-xc", "-Wno-implicit-function-declaration", filename, "-o", libname, NULL);
+#else
+        execlp("gcc", "gcc", "-fPIC", "-shared", "-m32", "-xc", "-Wno-implicit-function-declaration", filename, "-o", libname, NULL);
+#endif
+    }
+
+    int status;
+    wait(&status);
+    if (!WIFEXITED(status) || WEXITSTATUS(status)) {
+        return -1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     static char line[4096];
+
+    int n = 0;
+    char filename[32];
+    char libname[36];
+    char wrappername[32];
+    char buf[4160];
+    char *error;
+    int is_expr = 1;
+    void *handle;
+    int (*wrapper)();
 
     while (1) {
         printf("crepl> ");
@@ -12,7 +60,42 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        // To be implemented.
-        printf("Got %zu chars.\n", strlen(line));
+        strcpy(filename, "tmp/crepl_XXXXXX");
+        int fd = mkstemp(filename);
+        if (strncmp(line, "int", 3) == 0) {
+            is_expr = 0;
+            write(fd, line, strlen(line));
+        }
+        else {
+            is_expr = 1;
+            sprintf(wrappername, WRAPPER_NAME_FMT, n);
+            sprintf(buf, WRAPPER_FMT, n, line);
+            write(fd, buf, strlen(buf));
+        }
+        close(fd);
+
+        sprintf(libname, "%s.so", filename);
+        if (build_shared_lib(filename, libname) < 0) {
+            printf("compile error!\n");
+            continue;
+        }
+
+        handle = dlopen(libname, RTLD_LAZY | RTLD_GLOBAL);
+        if (!handle) {
+            continue;
+        }
+        dlerror();
+
+        if (is_expr) {
+             wrapper = dlsym(handle, wrappername);
+            if ((error = dlerror()) != NULL)  {
+                dlclose(handle);
+                continue;
+            }
+            printf("%d\n", wrapper());
+        }
+        else {
+            printf("OK.\n");
+        }
     }
 }
---
+++ crepl/tmp/crepl_22h69l.c
@@ -0,0 +1,1 @@
+int fact(n) { if ( n == 0) return n; else return fact(n-1) * n;}
